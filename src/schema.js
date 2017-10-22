0e3ea867912346654868f62859bce1dbe90d6a10
David Moody <david@davidxmoody.com>
2017-10-22 10:10:12 +0100

    Add markdown processing and wordcount

--- src/schema.js
+++ src/schema.js
@@ -1,27 +1,35 @@
 const {makeExecutableSchema} = require("graphql-tools")
+const marked = require("marked")
 
 const typeDefs = `
   type Entry {
     id: String!
     date: String!
-    rawContent: String
-    htmlContent: String
     deviceName: String
+    rawContent: String!
+    htmlContent: String
+    wordcount: Int
   }
 
   type Query {
+    entry(id: String!): Entry
     entries: [Entry]!
   }
 `
 
 const demoEntries = [
-  {id: "1", date: "2014-04-04", rawContent: "hello world", htmlContent: "<p>hello world</p>"},
+  {id: "1", date: "2014-04-04", rawContent: "hello *world*"},
   {id: "2", date: "2014-04-05", rawContent: "hello world 2"},
 ]
 
 const resolvers = {
   Query: {
+    entry: (_, {id}) => demoEntries.find((e) => e.id === id),
     entries: () => demoEntries,
+  },
+  Entry: {
+    wordcount: ({rawContent}) => rawContent.split(" ").length,
+    htmlContent: ({rawContent}) => marked(rawContent),
   },
 }
 
