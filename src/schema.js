33d3ade0aebdb962ba65e510981df64d4c7408a9
David Moody <david@davidxmoody.com>
2017-10-24 20:14:09 +0100

    Add calendar generation from life-calendar project

--- src/schema.js
+++ src/schema.js
@@ -1,26 +1,30 @@
 const {makeExecutableSchema} = require("graphql-tools")
 const getEntries = require("./getEntries")
+const getCalendar = require("./getCalendar")
+const lifeData = require("./lifeData.json")
+const fs = require("fs")
+const {join} = require("path")
+const moment = require("moment")
 
-const typeDefs = `
-  type Entry {
-    id: String!
-    date: String!
-    deviceName: String
-    rawContent: String!
-    htmlContent: String
-    wordcount: Int
-  }
+const typeDefs = fs.readFileSync(join(__dirname, "/schema.graphql")).toString()
 
-  type Query {
-    entry(id: String!): Entry
-    entries: [Entry]!
-  }
-`
+function getYears() {
+  const currentDate = moment().format("YYYY-MM-DD")
+  const {birthDate, deathDate, eras} = lifeData
+
+  return getCalendar({
+    currentDate,
+    birthDate,
+    deathDate,
+    eras,
+  }).years
+}
 
 const resolvers = {
   Query: {
     entry: (_, {id}) => getEntries().find((e) => e.id === id),
     entries: getEntries,
+    years: getYears,
   },
 }
 
